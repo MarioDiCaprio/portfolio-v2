10aac5ede78838cd3e7013c2ac49281db1454305
Mario Di Caprio <mariodicaprio10@gmail.com>
2025-03-31 13:19:01 +0200

    Added initial projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Page: React.FC = () => {
+    return (
+        <div className="h-full overflow-hidden mt-12 px-12">
+            <h1 className="title text-left">
+                Project Gallery
+            </h1>
+            <p>
+                Coming Soon!
+            </p>
+        </div>
+    );
+}
+
+export default Page;
