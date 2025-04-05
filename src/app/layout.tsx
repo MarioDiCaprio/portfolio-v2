816c21aa5a73b44108364b901fd88382cd712f26
Mario Di Caprio <mariodicaprio10@gmail.com>
2025-04-05 12:07:22 +0200

    Added app icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/app/Navbar";
 import Footer from "@/components/app/Footer";
+import Head from "next/head";
 
 
 const inter = Inter({
@@ -17,6 +18,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
+            <Head>
+                <link rel="icon" href="/icon.png" />
+            </Head>
             <body className={`${inter.className} antialiased bg-woodsmoke text-white min-h-screen flex flex-col`}>
                 <Navbar />
                 <main className="grow p-3">
